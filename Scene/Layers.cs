d562193391c3e71d14a8d390dcb95ea8dab13eba
Matthew Thompson <chatrat12@gmail.com>
2019-01-27 03:50:49 -0500

    Factored layers into its own file

---
+++ Scene/Layers.cs
@@ -0,0 +1,119 @@
+﻿
+namespace Terraria3D
+{
+    public static class Layers
+    {
+        public static void PopulateLayers(ref Layer3D[] layers)
+        {
+            if (layers != null)
+            {
+                foreach (var layer in layers)
+                    layer.Dispose();
+            }
+            layers = new Layer3D[]
+            {
+                new Layer3D()
+                {
+                    // Background
+                    ZPos = 16,
+                    Depth = 4,
+                    RenderFunction = () =>
+                    {
+                        Rendering.DrawBackgroundWater();
+                        Rendering.DrawSceneBackground();
+                        Rendering.DrawWalls();
+                    }
+                },
+                // Solid tiles
+                new Layer3D()
+                {
+                    RenderFunction = () =>
+                    {
+                        Rendering.DrawSolidTiles();
+                    }
+                },
+                new Layer3D()
+                {
+                    RenderFunction = () =>
+                    {
+                        Rendering.DrawForegroundWater();
+                    }
+                },
+                // Non Solid tiles
+                new Layer3D()
+                {
+                    ZPos = 12,
+                    Depth = 4,
+                    RenderFunction = () =>
+                    {
+                        Rendering.DrawNonSolidTiles();
+                        Rendering.DrawWaterFalls();
+
+                    }
+                },
+                //Player
+                new Layer3D()
+                {
+                    ZPos = 6,
+                    Depth = 8,
+                    NoiseAmount = 0,
+                    RenderFunction = () =>
+                    {
+                        Rendering.DrawPlayers();
+                        Rendering.DrawNPCsBehindTiles();
+                        Rendering.DrawNPCsBehindNonSoldTiles();
+                        Rendering.DrawNPCsInfrontOfTiles();
+                        Rendering.DrawNPCsOverPlayer();
+                        Rendering.DrawWallOfFlesh();
+                        Rendering.SortDrawCacheWorm();
+                    }
+                },
+                // Proj
+                new Layer3D()
+                {
+                    ZPos = 8,
+                    Depth = 1,
+                    NoiseAmount = 0,
+                    RenderFunction = () =>
+                    {
+                        Rendering.DrawProjectiles();
+                        Rendering.DrawProjsBehindNPCsAndTiles();
+                        Rendering.DrawProjsBehindProjectiles();
+                        Rendering.DrawProjsOverWireUI();
+                        Rendering.DrawNPCProjectiles();
+                        Rendering.DrawInfernoRings();
+
+                    }
+                },
+                // Items Gore
+                new Layer3D()
+                {
+                    ZPos = 7,
+                    Depth = 3,
+                    NoiseAmount = 0,
+                    RenderFunction = () =>
+                    {
+                        Rendering.DrawGore();
+                        Rendering.DrawBackGore();
+                        Rendering.DrawDust();
+                        Rendering.DrawRain();
+                        Rendering.DrawSandstorm();
+                        Rendering.DrawMoonLordDeath();
+                        Rendering.DrawMoonlordDeathFront();
+                        Rendering.DrawItems();
+                        Rendering.DrawHitTileAnimation();
+                        Rendering.DrawItemText();
+                        Rendering.DrawCombatText();
+                        Rendering.DrawChatOverPlayerHeads();
+                    }
+                },
+                new Layer3D()
+                {
+                    ZPos = -4,
+                    Depth = 4,
+                    RenderFunction = () => Rendering.DrawWires()
+                }
+            };
+        }
+    }
+}
--- Terraria3D.cs
+++ Terraria3D.cs
@@ -1,17 +1,7 @@
-using System.Collections.Generic;
-using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using MonoMod.RuntimeDetour.HookGen;
 using Terraria;
 using Terraria.ModLoader;
-using Mono.Cecil;
-using Terraria.UI;
-using System.Diagnostics;
-using System.Text;
-using System.IO;
-using System;
-using Mono.Cecil.Cil;
 
 namespace Terraria3D
 {
@@ -26,128 +16,15 @@ namespace Terraria3D
         public override void Load()
         {
             Instance = this;
-            InitLayers();
+            Layers.PopulateLayers(ref _layers);
             Hooks.Initialize();
-        }
-
-        private void InitLayers()
-        {
-            if (_layers != null)
-            {
-                foreach (var layer in _layers)
-                    layer.Dispose();
-            }
-            _layers = new Layer3D[]
-            {
-                new Layer3D()
-                {
-                    // Background
-                    ZPos = 16,
-                    Depth = 4,
-                    RenderFunction = () =>
-                    {
-                        Rendering.DrawBackgroundWater();
-                        Rendering.DrawSceneBackground();
-                        Rendering.DrawWalls();
-                    }
-                },
-                // Solid tiles
-                new Layer3D()
-                {
-                    RenderFunction = () =>
-                    {
-                        Rendering.DrawSolidTiles();
-                    }
-                },
-                new Layer3D()
-                {
-                    RenderFunction = () =>
-                    {
-                        Rendering.DrawForegroundWater();
-                    }
-                },
-                // Non Solid tiles
-                new Layer3D()
-                {
-                    ZPos = 12,
-                    Depth = 4,
-                    RenderFunction = () =>
-                    {
-                        Rendering.DrawNonSolidTiles();
-                        Rendering.DrawWaterFalls();
-
-                    }
-                },
-                //Player
-                new Layer3D()
-                {
-                    ZPos = 6,
-                    Depth = 8,
-                    NoiseAmount = 0,
-                    RenderFunction = () =>
-                    {
-                        Rendering.DrawPlayers();
-                        Rendering.DrawNPCsBehindTiles();
-                        Rendering.DrawNPCsBehindNonSoldTiles();
-                        Rendering.DrawNPCsInfrontOfTiles();
-                        Rendering.DrawNPCsOverPlayer();
-                        Rendering.DrawWallOfFlesh();
-                        Rendering.SortDrawCacheWorm();
-                    }
-                },
-                // Proj
-                new Layer3D()
-                {
-                    ZPos = 8,
-                    Depth = 1,
-                    NoiseAmount = 0,
-                    RenderFunction = () =>
-                    {
-                        Rendering.DrawProjectiles();
-                        Rendering.DrawProjsBehindNPCsAndTiles();
-                        Rendering.DrawProjsBehindProjectiles();
-                        Rendering.DrawProjsOverWireUI();
-                        Rendering.DrawNPCProjectiles();
-                        Rendering.DrawInfernoRings();
-
-                    }
-                },
-                // Items Gore
-                new Layer3D()
-                {
-                    ZPos = 7,
-                    Depth = 3,
-                    NoiseAmount = 0,
-                    RenderFunction = () =>
-                    {
-                        Rendering.DrawGore();
-                        Rendering.DrawBackGore();
-                        Rendering.DrawDust();
-                        Rendering.DrawRain();
-                        Rendering.DrawSandstorm();
-                        Rendering.DrawMoonLordDeath();
-                        Rendering.DrawMoonlordDeathFront();
-                        Rendering.DrawItems();
-                        Rendering.DrawHitTileAnimation();
-                        Rendering.DrawItemText();
-                        Rendering.DrawCombatText();
-                        Rendering.DrawChatOverPlayerHeads();
-                    }
-                },
-                new Layer3D()
-                {
-                    ZPos = -4,
-                    Depth = 4,
-                    RenderFunction = () => Rendering.DrawWires()
-                }
-            };
         }
 
         public override void PostDrawInterface(SpriteBatch spriteBatch)
         {
             Input.Update();
             if (Main.keyState.IsKeyDown(Keys.P))
-                InitLayers();
+                Layers.PopulateLayers(ref _layers);
             _scene.Update();
             //_settingsWinow.Draw(spriteBatch);
         }
@@ -162,11 +39,7 @@ namespace Terraria3D
 
         public void DrawScene()
         {
-            for (int i = 0; i < 20; i++)
-            {
-                _scene.Draw(_layers);
-
-            }
+            _scene.Draw(_layers);
         }
     }
 }
