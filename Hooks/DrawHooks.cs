93309f8cdb42390e703207427924fd73f5576e67
Matthew Thompson <chatrat12@gmail.com>
2019-01-27 22:39:57 -0500

    Refactored hooks

--- Hooks/DrawHooks.cs
+++ Hooks/DrawHooks.cs
@@ -2,6 +2,9 @@
 using Terraria.GameInput;
 using Microsoft.Xna.Framework.Graphics;
 using MonoMod.RuntimeDetour.HookGen;
+using Terraria;
+using Terraria.Graphics.Effects;
+using Microsoft.Xna.Framework;
 
 namespace Terraria3D
 {
@@ -13,31 +16,75 @@ namespace Terraria3D
             {
                 var cursor = il.At(0);
 
-                AddPreRenderHook(cursor);
-                AddSkip2DHook(cursor);
-                AddPostSceneRenderHook(cursor);
+                PreRenderHook(cursor);
+                StartCapture2DHook(cursor);
+                PostInWorldUIRenderHook(cursor);
+                DrawIfUIHiddenHook(cursor);
             };
         }
 
-        private static void AddPreRenderHook(HookILCursor cursor)
+        private static void PreRenderHook(HookILCursor cursor)
         {
             if (cursor.TryGotoNext(i => i.MatchLdcI4(4),
                                    i => i.MatchCall("Terraria.TimeLogger", "DetailedDrawTime")))
             {
                 cursor.Index += 3;
+                // Render layers to targets
                 cursor.EmitDelegate(() => Terraria3D.Instance.RenderLayersTargets());
             }
         }
 
-        private static void AddPostSceneRenderHook(HookILCursor cursor)
+        public static void StartCapture2DHook(HookILCursor cursor)
+        {
+            if (cursor.TryGotoNext(i => i.MatchLdcI4(1),
+                                   i => i.MatchLdcI4(1),
+                                   i => i.MatchCallvirt<OverlayManager>("Draw")))
+            {
+                int startIndex = cursor.Index;
+                cursor.Index++;
+                cursor.EmitDelegate(() =>
+                {
+                    // Don't capture if in main menu or full screen
+                    if (Main.gameMenu || Main.mapFullscreen) return;
+                    // Start capturing 2D draw calls
+                    Main.graphics.GraphicsDevice.SetRenderTarget(Renderers.ScreenTarget);
+                    Main.graphics.GraphicsDevice.Clear(Color.Transparent);
+                });
+            }
+        }
+
+        private static void PostInWorldUIRenderHook(HookILCursor cursor)
         {
             if (cursor.TryGotoNext(i => i.MatchCall<PlayerInput>("SetZoom_UI")))
             {
                 if (cursor.TryGotoPrev(i => i.MatchCallvirt<SpriteBatch>("End")))
                 {
                     cursor.Index++;
-                    cursor.EmitDelegate(() => Terraria3D.Instance.DrawScene());
+                    cursor.EmitDelegate(() =>
+                    {
+                        // Stop capturing draw calls
+                        Main.graphics.GraphicsDevice.SetRenderTarget(null);
+                        // Render the 3D scene
+                        Terraria3D.Instance.DrawScene();
+                    });
                 }
+            }
+        }
+
+        public static void DrawIfUIHiddenHook(HookILCursor cursor)
+        {
+            if (cursor.TryGotoNext(i => i.MatchLdcI4(37),
+                                      i => i.MatchCall("Terraria.TimeLogger", "DetailedDrawTime")))
+            {
+                cursor.Index++;
+                cursor.EmitDelegate(() =>
+                {
+                    // Stop capturing drawing
+                    Main.graphics.GraphicsDevice.SetRenderTarget(null);
+                    // If UI is hidden, the scene has not been drawn, do it now.
+                    if (Main.hideUI)
+                        Terraria3D.Instance.DrawScene();
+                });
             }
         }
     }
